e7ed1b51be4bf8fbb7b4cff3ccaebe5fb4fb03bb
yayza <y.aezah@gmail.com>
2022-08-06 21:46:48 -0700

    Add files

---
+++ server.js
@@ -0,0 +1,67 @@
+const { Client } = require("tplink-smarthome-api");
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const upload = multer({ dest: "/tmp/" });
+const smartHome = new Client();
+const settings = require("./settings.js");
+
+const clientList = {};
+smartHome.startDiscovery().on("device-new", (device) => {
+  device.getSysInfo().then((data) => (clientList[data.alias] = device));
+});
+
+function dimLights(brightness) {
+  try {
+    if (clientList[settings.DEVICE_NAME]._sysInfo.relay_state === 1) {
+      clientList[settings.DEVICE_NAME].send({
+        "smartlife.iot.dimmer": {
+          set_dimmer_transition: { brightness: brightness, mode: "instant_on_off", duration: 1 },
+        },
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function checkErrors(data) {
+  const isCorrectPlayer =
+    (settings.PLAYER_NAME.length > 0 && data.Player.title == settings.PLAYER_NAME) ||
+    settings.PLAYER_NAME.length == 0;
+  if (!clientList[settings.DEVICE_NAME]) {
+    console.log(`${settings.DEVICE_NAME} not found!`);
+    return false;
+  }
+  if (!isCorrectPlayer) return false;
+  if (!data.Player.local) return false;
+  return true;
+}
+
+function handleRequest(req) {
+  try {
+    const data = JSON.parse(req.body?.payload);
+    if (!checkErrors(data)) return;
+    const event = {
+      "media.play": { dim: settings.DIM, alias: "Played" },
+      "media.resume": { dim: settings.DIM, alias: "Resumed" },
+      "media.pause": { dim: settings.BRIGHTEN, alias: "Paused" },
+      "media.stop": { dim: settings.BRIGHTEN, alias: "Stopped" },
+    };
+    dimLights(event[data.event].dim);
+    console.log(
+      `${data.Metadata.title} ${event[data.event].alias}: Settings lights to ${
+        event[data.event].dim
+      }%`
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+app.post("/plex", upload.single("thumb"), (req, res) => {
+  handleRequest(req);
+  res.sendStatus(200);
+});
+
+app.listen(44010, () => console.log("Listening for Plex events"));
---
+++ settings.js
@@ -0,0 +1,9 @@
+// Settings
+const settings = {
+  DEVICE_NAME: "Yousef's Office", // Name of the lights to control
+  PLAYER_NAME: "", // Name of the device playing the media, leave empty to allow any player to dim the lights
+  DIM: 1,
+  BRIGHTEN: 20,
+};
+
+module.exports = settings;
