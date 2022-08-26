ad082e9b1952abed4be0708462543941ac819185
Jon Bianco <jon.bianco@revlon.com>
2022-08-26 13:18:44 -0400

    Dim to 0% Bugfix / Additional Parameters
    - Fixed bug if DIM is set to 0 in settings, will shut off light, but not turn back on
    - Added additional parameters in settings.js that allow DIM_MODE and DURATION

--- server.js
+++ server.js
@@ -8,60 +8,68 @@ const settings = require("./settings.js");
 
 const clientList = {};
 smartHome.startDiscovery().on("device-new", (device) => {
-  device.getSysInfo().then((data) => (clientList[data.alias] = device));
+    device.getSysInfo().then((data) => (clientList[data.alias] = device));
 });
 
 function dimLights(brightness) {
-  try {
-    if (clientList[settings.DEVICE_NAME]._sysInfo.relay_state === 1) {
-      clientList[settings.DEVICE_NAME].send({
-        "smartlife.iot.dimmer": {
-          set_dimmer_transition: { brightness: brightness, mode: "instant_on_off", duration: 1 },
-        },
-      });
+    if ((clientList[settings.DEVICE_NAME]._sysInfo.relay_state === 0) && (brightness !== 0)) {
+        console.log("Powering back on switch");
+        clientList[settings.DEVICE_NAME].send({
+            "smartlife.iot.dimmer": {
+                set_switch_state: { state: 1 },
+            },
+        });
     }
-  } catch (e) {
-    console.log(e);
-  }
+    try {
+        if (clientList[settings.DEVICE_NAME]._sysInfo.relay_state === 1) {
+            clientList[settings.DEVICE_NAME].send({
+                "smartlife.iot.dimmer": {
+                    set_dimmer_transition: { brightness: brightness, mode: settings.DIM_MODE, duration: settings.DURATION },
+                },
+            });
+        }
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 function checkErrors(data) {
-  const isCorrectPlayer =
-    (settings.PLAYER_UUID.length > 0 && data.Player.uuid == settings.PLAYER_UUID) ||
-    settings.PLAYER_UUID.length == 0;
+    const isCorrectPlayer =
+        (settings.PLAYER_UUID.length > 0 && data.Player.uuid == settings.PLAYER_UUID) ||
+        settings.PLAYER_UUID.length == 0;
 
-  if (!clientList[settings.DEVICE_NAME]) {
-    console.log(`${settings.DEVICE_NAME} not found!`);
-    return false;
-  }
-  if (!isCorrectPlayer || !data.Player.local) return false;
-  return true;
+    if (!clientList[settings.DEVICE_NAME]) {
+        console.log(`${settings.DEVICE_NAME} not found!`);
+        return false;
+    }
+    if (!isCorrectPlayer || !data.Player.local) return false;
+    return true;
 }
 
 function handleRequest(req) {
-  try {
-    const data = JSON.parse(req.body?.payload);
-    if (!checkErrors(data)) return;
-    const event = {
-      "media.play": { dim: settings.DIM, alias: "Played" },
-      "media.resume": { dim: settings.DIM, alias: "Resumed" },
-      "media.pause": { dim: settings.BRIGHTEN, alias: "Paused" },
-      "media.stop": { dim: settings.BRIGHTEN, alias: "Stopped" },
-    };
-    dimLights(event[data.event].dim);
-    console.log(
-      `Media ${event[data.event].alias}\nMedia Title: ${data.Metadata.title}\nPlayer UUID: ${
+    try {
+        const data = JSON.parse(req.body ? .payload);
+        if (!checkErrors(data)) return;
+        const event = {
+            "media.play": { dim: settings.DIM, alias: "Played" },
+            "media.resume": { dim: settings.DIM, alias: "Resumed" },
+            "media.pause": { dim: settings.BRIGHTEN, alias: "Paused" },
+            "media.stop": { dim: settings.BRIGHTEN, alias: "Stopped" },
+        };
+        dimLights(event[data.event].dim);
+        console.log(
+            `Media ${event[data.event].alias}\nMedia Title: ${data.Metadata.title}\nPlayer UUID: ${
         data.Player.uuid
       }\nSettings lights to ${event[data.event].dim}%\n`
-    );
-  } catch (e) {
-    console.log(e);
-  }
+        );
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 app.post("/plex", upload.single("thumb"), (req, res) => {
-  handleRequest(req);
-  res.sendStatus(200);
+    handleRequest(req);
+    res.sendStatus(200);
 });
 
 app.listen(44010, () => console.log("Listening for Plex events"));
